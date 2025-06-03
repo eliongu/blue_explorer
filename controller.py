2d2b77f5dcb4a270a41c0c1747922908c6c7fb06
ot664vts <luna.colombanfernandez@ynov.com>
2025-06-03 12:16:24 +0200

    git problems

--- controller.py
+++ controller.py
@@ -3,33 +3,36 @@ import espnow
 from machine import ADC, Pin
 import time
 
+# LEDs
 greenled = Pin(5, Pin.OUT)
 redled = Pin(4, Pin.OUT)
 
-sta = network.WLAN(network.WLAN.IF_STA)
+# Initialisation Wi-Fi
+sta = network.WLAN(network.STA_IF)
 sta.active(True)
 sta.disconnect()
 
+# Initialisation ESP-NOW
 e = espnow.ESPNow()
 e.active(True)
-
 
 macC6 = b'@L\xcaV\xbe,'
 e.add_peer(macC6)
 
-# Joystick
-vrx = ADC(Pin(3))  # Axe X
-vry = ADC(Pin(0))  # Axe Y
 
+vrx = ADC(Pin(3))  
+vry = ADC(Pin(0))  
 vrx.atten(ADC.ATTN_11DB)
 vry.atten(ADC.ATTN_11DB)
 
+
 sw = Pin(1, Pin.IN, Pin.PULL_UP)
 
-# Constantes
-DEAD_ZONE = 200  # zone morte pour éviter les variations
+
+DEAD_ZONE = 200
 CENTER = 2400
 
+# Fonction de direction
 def get_direction(x, y):
     if abs(x - CENTER) < DEAD_ZONE and abs(y - CENTER) < DEAD_ZONE:
         return "CENTRE"
@@ -43,28 +46,41 @@ def get_direction(x, y):
         return "DROITE"
     return "?"
 
+# Boucle principale
 while True:
-    host, msg = e.recv()
-    ledlight = msg.decode()
+    print("Début de boucle")
+    recv = e.recv()
+    print("Réception vérifiée")
+    if recv:
+        host, msg = recv
+        try:
+            ledlight = msg.decode()
+            if ledlight == "ROUGE":
+                print("LED Rouge")
+                redled.value(1)
+                greenled.value(0)
+            elif ledlight == "VERT":
+                print("LED Verte")
+                redled.value(0)
+                greenled.value(1)
+            else:
+                print("Aucune LED")
+        except Exception as ex:
+            print("Erreur de décodage :", ex)
+    else:
+        print("Aucun message reçu")
+
+    # Lecture joystick
     x = vrx.read()
     y = vry.read()
     sw_val = sw.value()
+
     direction = get_direction(x, y)
     bouton = "pressé" if sw_val == 0 else "relâché"
 
-    if ledlight == "ROUGE":
-        print("LED Rouge")
-        redled.value(1)
-        greenled.value(0)
-    elif ledlight == "VERT":
-        print("LED Verte")
-        redled.value(0)
-        greenled.value(1)
-    else:
-        print("Aucune LED")
-    
-    message = direction + "|" + bouton  
-
+    # Envoi du message
+    message = direction + "|" + bouton
     print("Envoi:", message)
     e.send(macC6, message)
+
     time.sleep(0.3)
